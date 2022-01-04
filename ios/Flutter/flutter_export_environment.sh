2f5a8e42578574446e99b5f2ef77162a3e6cc0d0
yongchulbag <yongchulbag@gmail.com>
2022-01-04 22:55:47 +0900

    잘모르는구문까지 확인

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\FlutterProject\xylophone-flutter"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\박용철\StudioProjects\xylophone-flutter3"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class XylophoneApp extends StatelessWidget {
   }
 
   Expanded lineCreate({Color color_select, int buttonNumber}) { // 전달 변수 두개를 {}로 안 감싸면 오류남
-    return Expanded(
-      child: ElevatedButton(
+    return Expanded(                                      // 찾아보니 원래 그런단다 구조체 this.이면 중괄호로 감싸라는데 뭔소리여 ㅋㅋ
+      child: ElevatedButton(                              // https://flutterforum.co/t/im-getting-this-error-too-few-positional-arguments-5-required-0-given/2099
         style: ElevatedButton.styleFrom(primary: color_select),
         onPressed: () {
           playSound(buttonNumber);
@@ -30,8 +30,8 @@ class XylophoneApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               lineCreate(color_select : Colors.red, buttonNumber: 1), // 여기에 변수명 지정 안하고 색깔과 숫자만 전달하면 오류남 이유가 멀까?
-              lineCreate(color_select : Colors.orange, buttonNumber: 2),
-              lineCreate(color_select : Colors.yellow, buttonNumber: 3),
+              lineCreate(color_select : Colors.orange, buttonNumber: 2), // 찾아보니 원래 플러터 다트에서는 변수명 만든거 있음 변수명도 적어줘야 된덴다....
+              lineCreate(color_select : Colors.yellow, buttonNumber: 3), // 출처 : https://www.quora.com/What-does-Error-Too-many-positional-arguments-0-expected-but-3-found-mean-in-Flutter-Dart
               lineCreate(color_select : Colors.green, buttonNumber: 4),
               lineCreate(color_select : Colors.teal, buttonNumber: 5),
               lineCreate(color_select : Colors.blue, buttonNumber: 6),
